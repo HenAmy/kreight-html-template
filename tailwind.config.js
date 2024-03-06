11f36809e59d25702a9f3ee4182b95f12686381c
Henry Amihere <66474328+HenAmy@users.noreply.github.com>
2024-03-06 22:35:01 +0000

    Initial development

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#01C38D",
+        secondary: "#132D46",
+        text: "#191E29",
+        gray: "#696E79",
+        placeholder: "#A0A0A0",
+        background: "#F8F7FA",
+        disabled: "#E2E2E2",
+        modal: "#FBFBFB",
+        notepad: "#F6E675",
+        error: "#FF0000",
+      },
+      fontFamily: {
+        mainFont: ['"Inter"', "sans-serif"],
+        // sans: ["Inter", "sans-serif"],
+      },
+      spacing: {
+        180: "32rem",
+      },
+    },
+  },
+  plugins: [],
+};
